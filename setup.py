8f2d1067d378d8fd3a2ae1fb3876534b2a59c64e
Przemyslaw Bubas <bprzemys@cisco.com>
2023-05-19 16:43:51 +0200

    function_argument unittest

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@
 from setuptools import setup
 
 setup(name='modbus-wrapper',
-    version='1.0.2b',
+    version='1.0.3',
     description='wrapper for pyModbusTCP accepting all Modbus numbers with Fatek support',
     author='Przemyslaw Bubas',
     author_email='bubasenator@gmail.com',
     url='https://github.com/pbubas/modbus_wrapper',
     install_requires=['pyModbusTCP'],
     keywords="modbus fatek",
-     )
+    python_requires='>=3.10'
+    )
