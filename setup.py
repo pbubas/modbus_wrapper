4daacd5db9e7f9df22e654fc647bac4358a672a8
Przemyslaw Bubas <bprzemys@cisco.com>
2023-05-19 17:48:03 +0200

    version bump

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='modbus-wrapper',
-    version='1.0.3',
+    version='1.0.4',
     description='wrapper for pyModbusTCP accepting all Modbus numbers with Fatek support',
     author='Przemyslaw Bubas',
     author_email='bubasenator@gmail.com',
