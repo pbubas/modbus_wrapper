6325cd4fa53a68a0c44139179dd4bac6d7a22b48
bubba <bubba@desktop.narama.lan>
2024-12-20 19:17:48 +0100

    package versions fix

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 
 setup(
     name='modbus-wrapper',
-    version='2.0.4',
+    version='2.0.4a',
     description='wrapper for pyModbusTCP accepting all Modbus numbers with Fatek support',
     author='Przemyslaw Bubas',
     author_email='bubasenator@gmail.com',
     url='https://github.com/pbubas/modbus_wrapper',
     install_requires=[
-        'pymodbus',
-        'pyserial'
+        'pymodbus==3.6.9',
+        'pyserial==3.5'
         ],
     keywords="modbus fatek",
     python_requires='>=3.10'
