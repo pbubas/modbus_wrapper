443148f57fd494caf2dc9cd963dcb59375b93e09
bubba <bubba@desktop.narama.lan>
2024-12-20 11:26:14 +0100

    fixed deps

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='modbus-wrapper',
-    version='2.0.3',
+    version='2.0.4',
     description='wrapper for pyModbusTCP accepting all Modbus numbers with Fatek support',
     author='Przemyslaw Bubas',
     author_email='bubasenator@gmail.com',
