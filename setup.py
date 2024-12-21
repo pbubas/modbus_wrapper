b8bde072052291d35c6f4533411c2b0975cda688
bubba <bubba@desktop.narama.lan>
2024-12-21 18:16:54 +0100

    version of modbus library decreased to compatible

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ from setuptools import setup
 
 setup(
     name='modbus-wrapper',
-    version='2.0.4a',
+    version='2.0.5',
     description='wrapper for pyModbusTCP accepting all Modbus numbers with Fatek support',
     author='Przemyslaw Bubas',
     author_email='bubasenator@gmail.com',
     url='https://github.com/pbubas/modbus_wrapper',
     install_requires=[
-        'pymodbus==3.6.9',
+        'pymodbus==3.5.2',
         'pyserial==3.5'
         ],
     keywords="modbus fatek",
